c3c35a42bfb0b3c53b2f1d7cf82f699c44c40f43
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-05-26 01:18:12 +0100

    init

---
+++ GameJam'24-LastStand/Assets/Scripts/Joystick.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# gamejam24
+# GameJam'24 by GameDev.tv - https://itch.io/jam/gamedevtv-jam-2024 
+## Theme - Last Stand
+
+
+To-Do
+
+-[ ] Movement with joystick using interfaces
